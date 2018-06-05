fb276bec2cb00dfc1324fa03058ac03524e5ac5b
ycli <ycli@stanford.edu>
2018-06-04 19:47:06 -0700

    Initial commit

---
+++ HandSelection/Assets/CubeControlCapsule.cs
@@ -0,0 +1,185 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+//using System.Net.Sockets;
+using System.Text;
+//using System.Threading;
+//using System.Linq;
+
+namespace Meta.HandInput.Utility
+{
+
+    /// <summary>
+    /// Script to update a MetaCube's material as it changes states.
+    /// </summary>
+    public class CubeControlCapsule : MonoBehaviour
+    {
+        /// <summary>
+        /// The Interaction script to listen to.
+        /// </summary>
+        public Interaction Interaction;
+
+        /*
+        public MaterialState[] Materials = new MaterialState[3]
+        {
+            new MaterialState(2, new Color(0.5f, 0.5f, 0.5f, 1f), Color.white, new Color(1f, 2f, 3f)),
+            new MaterialState(4, new Color(0.5f, 0.5f, 0.5f, 1f), Color.white, new Color(1f, 2f, 3f)),
+            new MaterialState(6, Color.clear, new Color(0f, 0.55f, 0.55f, 1f), Color.white),
+        }; */
+
+        //private Rigidbody rb;
+        private GameObject cp0, cp1;
+
+        private bool cp0_on, cp1_on;
+
+        // Use this for initialization
+        void Start()
+        {
+            if (Interaction == null) { Interaction = GetComponent<Interaction>(); }
+            if (Interaction == null)
+            {
+                Debug.LogWarning("ToggleVisualCube's Interaction Object has not been configured. CubeControlCapsule won't execute.");
+                return;
+            }
+            //rb = GetComponent<Rigidbody>();
+            //rb.SetVisibility(false);
+            cp0 = GameObject.FindWithTag("Capsule0");
+            cp0_on = cp0.activeSelf;
+            //cp0.SetActive(false);
+            cp1 = GameObject.FindWithTag("Capsule1");
+            cp1_on = cp1.activeSelf;
+            //cp1.SetActive(false);
+
+            // -- Subscribe to the Interaction script's events
+            Interaction.Events.HoverStart.AddListener(OnHoverStart);
+            Interaction.Events.HoverEnd.AddListener(OnHoverEnd);
+            Interaction.Events.Engaged.AddListener(OnGrabStart);
+            Interaction.Events.Disengaged.AddListener(OnGrabEnd);
+
+        }
+
+
+
+        void Update()
+        {
+            //UpdateColors();
+        }
+
+        private void OnHoverStart(MetaInteractionData data)
+        {
+            //UpdateStateVisuals(PalmState.Hovering);
+        }
+
+        private void OnHoverEnd(MetaInteractionData data)
+        {
+            //UpdateStateVisuals(PalmState.Idle);
+        }
+
+        private void OnGrabStart(MetaInteractionData data)
+        {
+            //UpdateStateVisuals(PalmState.Grabbing);
+        }
+
+        private void OnGrabEnd(MetaInteractionData data)
+        {
+            //UpdateStateVisuals(PalmState.Hovering);
+            Debug.Log("GrabEnd.....");
+            cp0_on = !cp0_on;
+            cp0.SetActive(cp0_on);
+            cp1_on = !cp1_on;
+            cp1.SetActive(cp1_on);
+        }
+
+        /*
+        private void UpdateStateVisuals(PalmState newState)
+        {
+            foreach (var materialState in Materials)
+            {
+                materialState.SetColor(newState);
+            }
+        }
+
+        private void UpdateColors()
+        {
+            foreach (var materialState in Materials)
+            {
+                materialState.Update();
+            }
+        }
+        
+        /*
+        [System.Serializable]
+        public class MaterialState
+        {
+            [Readonly]
+            public int MaterialIndex;
+            [Readonly]
+            [ColorUsage(false, true, 0f, 5f, 0f, 5f)]
+            public Color Idle;
+
+            [Readonly]
+            [ColorUsage(false, true, 0f, 5f, 0f, 5f)]
+            public Color Highlight;
+
+            [Readonly]
+            [ColorUsage(false, true, 0f, 5f, 0f, 5f)]
+            public Color Active;
+
+            private Renderer _renderer;
+
+            private Color _targetColor;
+            private Color _previousColor;
+
+            private float _interpAmount;
+            private float _interpAmountVelocity;
+
+            private const float SmoothTime = 0.05f;
+
+            public MaterialState(int materialIndex, Color idle, Color highlight, Color active)
+            {
+                MaterialIndex = materialIndex;
+
+                Idle = idle;
+                Highlight = highlight;
+                Active = active;
+            }
+
+            public void Initialize(MonoBehaviour behaviour)
+            {
+                _renderer = behaviour.GetComponent<Renderer>();
+
+                // -- Initialize variables to default 'idle' state.
+                SetColor(PalmState.Idle);
+            }
+
+            public void SetColor(PalmState state)
+            {
+                switch (state)
+                {
+                    case PalmState.Idle:
+                        _targetColor = Idle;
+                        break;
+                    case PalmState.Hovering:
+                        _targetColor = Highlight;
+                        break;
+                    case PalmState.Grabbing:
+                        _targetColor = Active;
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("state", state, null);
+                }
+
+                _previousColor = _renderer.materials[MaterialIndex].GetColor("_EmissionColor");
+                _interpAmount = 0f;
+            }
+
+            public void Update()
+            {
+                _interpAmount = Mathf.Clamp01(Mathf.SmoothDamp(_interpAmount, 1f, ref _interpAmountVelocity, SmoothTime));
+                _renderer.materials[MaterialIndex].SetColor("_EmissionColor", Color.Lerp(_previousColor, _targetColor, _interpAmount));
+            }
+        }
+        */
+    }
+}
---
+++ SphereControl/Assets/Controller.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Linq;
+
+public class Controller : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private TcpClient socketConnection;
+    private Thread clientReceiveThread;
+    private float x, y, z;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // ListenForData();
+        ConnectToTcpServer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 movement = new Vector3(x, y, z);
+        rb.MovePosition(movement / 10);
+
+        if (Input.GetKey("up"))
+        {
+            movement = new Vector3(0.0f, 2.0f, 0.0f);
+            rb.AddForce(movement * 3);
+
+        }
+    }
+
+
+    /// <summary> 	
+
+    /// Setup socket connection. 	
+
+    /// </summary> 	
+
+    private void ConnectToTcpServer()
+    {
+
+        try
+        {
+
+            clientReceiveThread = new Thread(new ThreadStart(ListenForData));
+
+            clientReceiveThread.IsBackground = true;
+
+            clientReceiveThread.Start();
+
+        }
+
+        catch (Exception e)
+        {
+
+            Debug.Log("On client connect exception " + e);
+
+        }
+
+    }
+
+    /// <summary> 	
+
+    /// Runs in background clientReceiveThread; Listens for incomming data. 	
+
+    /// </summary>     
+
+    private void ListenForData()
+    {
+
+        try
+        {
+
+            socketConnection = new TcpClient("10.34.160.167", 8080);
+
+            Byte[] bytes = new Byte[128];
+
+            while (true)
+            {
+
+                // Get a stream object for reading 				
+
+                using (NetworkStream stream = socketConnection.GetStream())
+                {
+
+                    byte[] myWriteBuffer = Encoding.ASCII.GetBytes("GET / HTTP/1.1\nHost: 10.34.160.167:8081\n\n");
+                    stream.Write(myWriteBuffer, 0, myWriteBuffer.Length);
+
+                    int length;
+
+                    // Read incomming stream into byte arrary. 					
+
+                    while ((length = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+
+                        var incommingData = new byte[length];
+
+                        Array.Copy(bytes, 0, incommingData, 0, length);
+
+                        // Convert byte array to string message. 						
+
+                        string serverMessage = Encoding.ASCII.GetString(incommingData);
+
+                        Debug.Log("server message received as: " + serverMessage);
+
+                        string[] split = serverMessage.Split('/');
+                        if (split.Length < 3)
+                            continue;
+                     
+                        string xyz = split[1];
+                        
+                        string[] xyz_array = xyz.Split(',');
+                        if (xyz_array.Length != 3)
+                            continue;
+                        Debug.Log(xyz);
+
+                        x = float.Parse(xyz_array[0]);
+                        y = float.Parse(xyz_array[1]);
+                        z = float.Parse(xyz_array[2]);
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        catch (SocketException socketException)
+        {
+
+            Debug.Log("Socket exception: " + socketException);
+
+        }
+
+    }
+}
